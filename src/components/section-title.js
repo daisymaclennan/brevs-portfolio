fd89c17e3b10dfdf60b13ab46cd12812cf86f39b
Daisy Maclennan <daisy@sidigital.co>
2020-02-11 09:34:58 +0000

    Title word wrapping added

--- src/components/section-title.js
+++ src/components/section-title.js
@@ -5,20 +5,23 @@ const SectionTitle = styled.h3`
   display: block;
   margin-top: 150px;
   color: ${props => props.theme.colors.titleSolid};
-  @media screen and (min-width: 1000px){
-    grid-column-start: 2;
-    grid-column-end: 13;
-    white-space: nowrap;
-
-    margin-bottom: 90px;
-  }
 
   @media screen and (max-width: 1000px){
     grid-column-start: 1;
     grid-column-end: 5;
     line-height: 100%;
     margin-bottom: 60px;
+    word-wrap: break-word;
   }
+
+  @media screen and (min-width: 1000px){
+    grid-column-start: 2;
+    grid-column-end: 13;
+    margin-bottom: 90px;
+    white-space: nowrap;
+  }
+
+
 `
 
 export default SectionTitle
