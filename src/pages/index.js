faa93cc781b6ea6308715f88a88f65ad2d1b72c7
daisymaclennan <46746417+daisymaclennan@users.noreply.github.com>
2020-02-13 09:42:34 +0000

    Remove old component in comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,11 +57,6 @@ export default () => (
         description="I was directly involved within the website redesign, Smartdriverclub Insurance wanted a fresh new look for their website. This involved building a design system for the developers to work from aswell as thinking through users flows of how the website will be used to better conversion rate."
         image="sdc.png"
       />
-      {/*<ContentWithImageDark
-        title="Wireframes"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-        image="limbo-wireframes.png"
-      />*/}
       <Team team={ smartdriverclubTeam } />
       {/*Limbo*/}
       <Project
